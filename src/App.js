e72db288e7a3acb55cbd92df4a6356ff83a51374
Mark Gauffin <markgauffin@gmail.com>
2022-06-01 06:09:26 -0500

    project1 setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import Application from './projects/project1/Application';
+
+function App() {
+    return <Application />;
+}
+
+export default App;
---
+++ src/projects/project1/Application.js
@@ -0,0 +1,11 @@
+import Navbar from './Navbar';
+import Main from './Main';
+
+export default function Application() {
+    return (
+        <>
+            <Navbar />
+            <Main />
+        </>
+    );
+}

377c58d1f4291eb5824c29eb78f8ea2fd9f1ba39
Mark Gauffin <markgauffin@gmail.com>
2022-06-07 05:59:02 -0500

    create About and Interest components

--- src/projects/digital-business-card/components/About.js
+++ src/projects/digital-business-card/components/About.js
@@ -1,3 +1,13 @@
 export default function About() {
-    return <p>About section</p>;
+    return (
+        <>
+            <h2 className="text-header">About</h2>
+            <p className="text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Tenetur eos earum sunt necessitatibus nulla harum tempora
+                inventore labore. Inventore tempore molestiae est deserunt
+                tenetur veritatis optio quas? Nisi, expedita officiis.
+            </p>
+        </>
+    );
 }
--- src/projects/digital-business-card/components/Interest.js
+++ src/projects/digital-business-card/components/Interest.js
@@ -1,3 +1,13 @@
 export default function Interest() {
-    return <p>Interest section</p>;
+    return (
+        <>
+            <h2 className="text-header">Interest</h2>
+            <p className="text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure
+                repellat labore cum voluptatem magnam tenetur natus dignissimos.
+                Deserunt ipsa recusandae molestiae exercitationem qui vero
+                natus, earum cumque omnis, ex est.
+            </p>
+        </>
+    );
 }
